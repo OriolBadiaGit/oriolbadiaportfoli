f8701e4bce37199de2726fcbf04505d0e690467d
OriolBadiaGit <oriol.badia.mallorqui@uvic.cat>
2025-10-26 16:53:37 +0100

    retoquets

--- Plugins/Tippy/PersonalTippy.js
+++ Plugins/Tippy/PersonalTippy.js
@@ -1,5 +1,7 @@
-// With the above scripts loaded, you can call `tippy()` with a CSS
-      // selector and a `content` prop:
       tippy('#BtnCVsect', {
         content: 'Encara no acabat',
+        font
       });
+
+document.getElementById(BtnCVsect);
+
